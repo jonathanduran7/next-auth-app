766be3242ccdc6cd74a198a28b89ecee331a6684
jonathanduran7 <joniduran624@gmail.com>
2024-08-01 21:58:16 -0300

    refactor navbar component

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -2,30 +2,30 @@
 
 import { signIn, signOut, useSession } from "next-auth/react"
 
-export default function Navbar(){
+export default function Navbar() {
 
   const { data: session } = useSession();
 
   return (
-  <div className="bg-black text-white w-full h-16 flex justify-between p-5">
+    <div className="bg-black text-white w-full h-16 flex justify-between p-5">
       <div>
-        app
+        Notes App
       </div>
       <div>
         {
           session ? (
-            <button 
+            <button
               onClick={async () => {
-              await signOut({
-                callbackUrl: "/",
-              })
+                await signOut({
+                  callbackUrl: "/",
+                })
               }
-            }>
-              Welcome {session.user?.name} - Logout
+              }>
+              Logout
             </button>
           ) : (
             <button onClick={() => signIn()}>
-              Login With Google
+              Login
             </button>
           )
         }
